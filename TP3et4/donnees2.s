f31206e450742faa23d0da072a74cb3c6a26d3a2
Vasilis Skarleas <51437323+vskarleas@users.noreply.github.com>
2023-03-06 18:59:27 +0100

    .

---
+++ TP3et4/donnees2.s
@@ -0,0 +1,10 @@
+    .data
+aa: .word 65    @ .byte 0x41
+oo: .word 15   @ .byte  0x0f
+cc: .asciz "bonjour"
+rr: .word 66   @ .byte 0x42
+    .word 3
+T: .word 0x1122
+   .word 0x3456
+   .word  0xfafd
+xx: .word 65
